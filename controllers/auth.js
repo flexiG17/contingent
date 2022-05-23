3d8e3ef9c8ec8ab4d3d21512035728245efd5b26
flexiG17 <mister23krut@gmail.com>
2022-05-23 22:18:48 +0500

    create registration with bcrypt password and with verification user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,9 @@
-const db = require('../db');
+const bcrypt = require('bcryptjs')
+
 const User = require('../models/User')
-// const User = require('../models/User')
 
 // req - все данные, которые отправляет пользователь
-module.exports.login = function (req, res){
+module.exports.login = function (req, res) {
     res.status(200).json({
         login: {
             email: req.body.email,
@@ -13,27 +13,41 @@ module.exports.login = function (req, res){
 }
 
 //req.body.email or req.body.password - получать данные от пользователя
-module.exports.register = function (req, res){
-    const userId = req.body.id
+module.exports.register = async function (req, res) {
+
     const userEmail = req.body.email
     const userPassword = req.body.password
     const userName = req.body.name
 
-    const user = new User(userId, userEmail, userPassword, userName)
+    const salt = bcrypt.genSaltSync(10)
 
-    user.save()
-        .then(() => console.log(`added to database ${user.name} with email ${user.email}`))
+    const user = new User(
+        userEmail,
+        bcrypt.hashSync(userPassword, salt),
+        userName)
 
-    res.status(200).json({
-        message: `Created new user ${user.name}`
+    user.isExist().then(userExistsInSystem => {
+        if (userExistsInSystem) {
+            res.status(409).json({
+                message: `User ${user.name} exists in system. Try again`
+            })
+
+            console.log(`User ${user.name} exists in system`)
+        } else {
+            user.save()
+                .catch()
+                .then(() => {
+                    res.status(201).json(user)
+                    console.log(`It\`s a new user. Added to database ${user.name} with email ${user.email}`)
+                })
+        }
     })
 }
 
-module.exports.changeData = function (req, res){
+module.exports.changeData = function (req, res) {
 
 }
 
-module.exports.getById = async function (req, res){
-    const data = await db('users').where({email: 'senya@mail.ru'});
-    console.log(data)
+module.exports.getById = async function (req, res) {
+
 }
--- models/User.js
+++ models/User.js
@@ -2,15 +2,25 @@ const db = require('../db');
 
 module.exports = class User {
 
-    constructor(id, email, password, name) {
-        this.id = id
+    constructor(email, password, name) {
         this.email = email;
         this.password = password;
         this.name = name;
     }
 
     async save() {
-        await db('users').insert(
-            [{id: this.id, email: this.email, password: this.password, name: this.name}])
+        await db('users').insert([
+            {
+                id: Math.floor(Math.random() * 10000000),
+                email: this.email,
+                password: this.password,
+                name: this.name
+            }])
+    }
+
+    async isExist() {
+        const database = await db('users').where({email: this.email})
+
+        return database.length !== 0
     }
 }
