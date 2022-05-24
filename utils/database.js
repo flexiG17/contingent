c3168000ff1b56fb8ddba7be4eda7257b7beafb6
flexiG17 <mister23krut@gmail.com>
2022-05-25 00:19:41 +0500

    added getAllData, remove and getCurrentData methods for working with database

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,24 @@
 const db = require('../db')
 
-module.exports.isExist = async function (databaseName, param) {
-    const database = await db(databaseName).where(param)
+module.exports.isExist = async function (databaseName, condition) {
+    const database = await db(databaseName).where(condition)
 
     return database.length !== 0
 }
 
 module.exports.save = async function (databaseName, param) {
     await db(databaseName).insert([param])
+}
+
+module.exports.getAllData = async function(databaseName){
+    return await db(databaseName)
+}
+
+module.exports.remove = async function(databaseName, condition){
+    await db(databaseName).where(condition).del()
+}
+
+// мб всё-таки добавить await (среда предлагает убрать)
+module.exports.getCurrentData = async function(databaseName, currentColumns){
+    return db(databaseName).select(currentColumns)
 }
