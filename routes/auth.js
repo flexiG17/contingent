77a160cdf3eb1d9f5d6c541a650d3aed766b9d90
flexiG17 <mister23krut@gmail.com>
2022-05-25 16:53:44 +0500

    code refactoring

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express')
 const controller = require('../controllers/auth')
 const router = express.Router()
 
-
 // localhost:5000/api/auth/login
 router.post('/login', controller.login)
 
@@ -15,18 +14,4 @@ router.patch('/change', controller.changeData)
 // get user data (name) for display on pages
 router.get('/:id', controller.getById)
 
-/*
-// localhost:5000/api/auth/getPerson
-router.post('/getPerson', (req, res, next) => {
-    controller.GetPerson(req.body)
-        .then(data => res.status(200).json({
-            message: 'Запрос выполнен успешно',
-            data
-        }))
-        .catch(err => next(err));
-});
-*/
-
-
-// Экспортируем
 module.exports = router
