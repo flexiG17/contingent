f00749ea30f320d37d8ec69440e3152615ed49d4
flexiG17 <mister23krut@gmail.com>
2022-05-29 21:14:10 +0500

    created the ability to register a person

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './LoginPage.css';
 import Logo from './logo.png';
 import {NavLink} from "react-router-dom";
+import {setUser} from '../services/serverData'
 
 function LoginPage() {
+    const [inputName, setNameItemInput] = useState('')
+    const [inputEmail, setEmailItemInput] = useState('')
+    const [inputPassword, setPasswordItemInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: inputName,
+            email: inputEmail,
+            password: inputPassword
+        }
+        setUser(data)
+    };
+
     return (
         <>
             <div className="form_container">
@@ -11,16 +26,22 @@ function LoginPage() {
                     <div className="Login_Logo"><img src={Logo} width="250px" height="50px" alt=""/></div>
 
 
-                    <form className="form_style">
-                        <label className="label_style" htmlFor="email">Имя пользователя или email</label>
-                        <input className="input_style" name="email" type="text" placeholder="Введите свой логин"/>
+                    <form className="form_style" onSubmit={handleSubmit}>
+                        <label className="label_style" htmlFor="email">Имя пользователя</label>
+                        <input className="input_style" name="email" type="text" placeholder="Введите своё имя"
+                               onChange={event => setNameItemInput(event.target.value)} value={inputName}/>
+                        <label className="label_style" htmlFor="email">Email</label>
+                        <input className="input_style" name="email" type="email" placeholder="Введите свой email"
+                               onChange={event => setEmailItemInput(event.target.value)} value={inputEmail}/>
                         <label className="label_style" htmlFor="password">Пароль</label>
                         <input className="input_style" name="password" type="password"
-                               placeholder="Введите свой пароль"/>
-                        <label className="checkbox_style_login"><input type="checkbox"
-                                                                       placeholder="Запомните меня"/> Запомните меня
+                               placeholder="Введите свой пароль"
+                               onChange={event => setPasswordItemInput(event.target.value)} value={inputPassword}/>
+                        <label className="checkbox_style_login">
+                            <input type="checkbox" placeholder="Запомните меня"/> Запомните меня
                         </label>
-                        <NavLink to="/HomePage" className="button_style_login"> Войти </NavLink>
+                        <NavLink onSubmit={handleSubmit} to="/HomePage" className="button_style_login"> Войти </NavLink>
+                        <button type="submit" className="button_style_login" > Войти</button>
                     </form>
                 </div>
             </div>
