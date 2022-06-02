d9a1421ebaed3eae81f36a5817b381b27817f348
flexiG17 <mister23krut@gmail.com>
2022-06-02 18:36:19 +0500

    Added a new js file to protect routes

---
+++ server/middleware/passport.js
@@ -0,0 +1,30 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const keys = require('../config/keys')
+const database = require('../utils/database')
+
+
+const options = {
+    // берём токен из Header
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.jwt
+}
+
+module.exports = passport => {
+    passport.use(
+        new JwtStrategy(options, (payload, done) => {
+                database.isExist('users', {id: payload.userId})
+                    .then(userExistInSystem => {
+                        if (userExistInSystem){
+                            const user = database.getDataWithSelectedColumns('users', {id: payload.userId}, ['email', 'id'])
+                            done(null, user)
+                        } else {
+                            done(null, false)
+                        }
+                    })
+                    .catch(error => console.log(error))
+            }
+        )
+    )
+}
