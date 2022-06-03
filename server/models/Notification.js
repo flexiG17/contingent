fa651c5c07abfa9eeecbc235ffd9ce18a1767bba
flexiG17 <mister23krut@gmail.com>
2022-06-03 11:23:40 +0500

    Created Notification model

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,10 +1,20 @@
 module.exports= class Notification{
 
-    constructor(notification_id, type, student_name, date, status){
-        this.id = notification_id;
-        this.type = type;
-        this.student_name = student_name;
-        this.date = date;
-        this.status = status;
+    constructor(req){
+        this.type = req.body.type;
+        this.student_name = req.body.student_name;
+        this.date = req.body.date;
+        this.status = req.body.status;
+        this.userId = req.body.userId
+    }
+
+    getModel(){
+        return{
+            type: this.type,
+            student_name: this.student_name,
+            date: this.date,
+            status: this.status,
+            user_id: this.userId
+        }
     }
 }
