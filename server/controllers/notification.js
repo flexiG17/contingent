0af6acea5ec03cc9b4730a947744fea3014f6c44
flexiG17 <mister23krut@gmail.com>
2022-06-04 16:58:15 +0500

    Realized create and getByUserId methods

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -1,19 +1,28 @@
 const Notification = require('../models/Notification')
+const database = require('../utils/database')
 
-module.exports.create = function (req, res){
-    res.status(200).json({
-        notification: req.body
-    })
+const databaseName = 'notifications'
+
+module.exports.create = async (req, res) => {
+    const [user] = await req.user
+
+    const notification = new Notification(req, user)
+    database.save(databaseName, notification.getModel())
+        .then(() => {
+            res.status(200).json({message: "successfully"})
+        })
 }
 
 module.exports.update = function (req, res){
-    res.status(200).json({
-        update: req.body
-    })
 }
 
-module.exports.getByUserId = function (req, res){
+module.exports.getByUserId = async function (req, res){
+    const [user] = await req.user
 
+    database.getOneField(databaseName, {user_id: user.id})
+        .then(data => {
+            res.status(200).send(data)
+        })
 }
 
 module.exports.remove = function (req, res){
