caf5f80984f8fccd007685a0cf3b68f51f8c2e96
grigoryblack <bathedultrapro@yandex.ru>
2022-06-05 20:19:51 +0500

    добавление студента,+добавление чекбокса

--- client/src/components/common/Contract/DocContract.jsx
+++ client/src/components/common/Contract/DocContract.jsx
@@ -1,38 +1,98 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./DocContract.css";
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import './EducationContract.css';
+import './InfoContacts.css';
 
 
 function DocContract() {
+    const[active, setActive] = useState(true);
+    const handleClickContract = () =>{
+        setActive(!active)
+    }
    
     return (
             <>
-            <div className="columns_position">
-                <div className="column_style_contract">
-                    <p className="tytle_contract_doc"> Уровень образования</p>
-                    <input type="text" placeholder="Дата въезда" className="input_style_contract"/>
-                    <input type="text" placeholder="Срок действия визы" className="input_style_contract"/>
-                    <input type="text" placeholder="Дата передачи в международную службу" className="input_style_contract"/>
-                    <input type="text" placeholder="Дата передачи в МВД" className="input_style_contract"/>
-                    <input type="text" placeholder="Ориентировочная дата получения" className="input_style_contract"/>
-                    <input type="text" placeholder="Фактическая дата получения приглашения" className="input_style_contract"/>
-                </div> 
-                <div className="column_style_contract">
-                    <p className="tytle_contract_doc_contaner"> Документы для загрузки в личную карточку контрактника </p>
-                    <p className="Doc_list">1) Фото паспорта со сроком действия (.PDF)</p>
-                    <p className="Doc_list">2) Перевод паспорта на русский язык (.PDF)</p>
-                    <p className="Doc_list">3) Визовая анкета (.PDF)</p>
-                    <p className="Doc_list">4) Документ, подтверждающий факт оплаты (.PDF)</p>
-                    <p className="Doc_list">5) Удостоверение личности(.PDF)</p>
-                    <p className="Doc_list">6) Перевод удостоверения личности(.PDF)</p>
-                    <input type="file" id="actual-btn" hidden/>
-                    <label for="actual-btn" className="label_doc"> Выберите файлы <InsertDriveFileIcon sx={{ fontSize: 20 }}/></label>
-                </div> 
-            </div> 
-            <div className="button_position_contract_doc">
-                <button type="submit" className="button_style_contract_doc">Добавить</button>
+            <div className="info_and_education_contaner">
+                <div className="columns_position">
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_info"> Личные данные</p>
+                        <input type="text" placeholder="Имя" className="input_style_contract"/>
+                        <input type="text" placeholder="Фамилия" className="input_style_contract"/>
+                        <input type="text" placeholder="Name" className="input_style_contract"/>
+                        <input type="text" placeholder="Secont name" className="input_style_contract"/>
+                        <p className="tytle_contract_info"> Контактные данные</p>
+                        <input type="text" placeholder="Контактный телефон" className="input_style_contract"/>
+                        <input type="text" placeholder="E-mail" className="input_style_contract"/>
+                    </div>
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_info"> Паспортные данные </p>
+                        <input type="text" placeholder="Страна" className="input_style_contract"/>
+                        <input type="text" placeholder="Место рождения" className="input_style_contract"/>
+                        <input type="text" placeholder="Дата рождения" className="input_style_contract"/>
+                        <input type="text" placeholder="Место проживания" className="input_style_contract"/>
+                        <input type="text" placeholder="Гражданство" className="input_style_contract"/>
+                        <input type="text" placeholder="Пол" className="input_style_contract"/>
+                        <input type="text" placeholder="Номер паспорта" className="input_style_contract"/>
+                        <input type="text" placeholder="Срок действия паспорта" className="input_style_contract"/>
+                        <input type="text" placeholder="Кем выдан" className="input_style_contract"/>
+                        <input type="text" placeholder="Дата выдачи" className="input_style_contract"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="info_and_education_contaner">
+                <div className="columns_position">
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_education"> Уровень образования</p>
+                        <input type="text" placeholder="Уровень полученного образования" className="input_style_contract"/>
+                        <input type="text" placeholder="Наименование учебного заведения" className="input_style_contract"/>
+                        <input type="text" placeholder="Адрес учебного заведения" className="input_style_contract"/>
+                        <input type="text" placeholder="Год окончания" className="input_style_contract"/>
+                        <input type="text" placeholder="Область образования" className="input_style_contract"/>
+                        <input type="text" placeholder="Контактный телефон" className="input_style_contract"/>
+                    </div>
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_education"> Нынешнее образование </p>
+                        <input type="text" placeholder="Уровень желаемого образования" className="input_style_contract"/>
+                        <input type="text" placeholder="Форма обучения" className="input_style_contract"/>
+                        <input type="text" placeholder="Образовательная организация" className="input_style_contract"/>
+                    </div>
+                </div>
+            </div>
+            <div className="info_and_education_contaner">
+                <div className="columns_position">
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_doc"> Уровень образования</p>
+                        <input type="text" placeholder="Дата въезда" className="input_style_contract"/>
+                        <input type="text" placeholder="Срок действия визы" className="input_style_contract"/>
+                        <input type="text" placeholder="Дата передачи в международную службу" className="input_style_contract"/>
+                        <input type="text" placeholder="Дата передачи в МВД" className="input_style_contract"/>
+                        <input type="text" placeholder="Ориентировочная дата получения" className="input_style_contract"/>
+                        <input type="text" placeholder="Фактическая дата получения приглашения" className="input_style_contract"/>
+                    </div>
+                    <div className="column_style_contract">
+                        <p className="tytle_contract_doc_contaner"> Документы для загрузки в личную карточку контрактника </p>
+                        <p className="Doc_list">1) Фото паспорта со сроком действия (.PDF)</p>
+                        <p className="Doc_list">2) Перевод паспорта на русский язык (.PDF)</p>
+                        <p className="Doc_list">3) Визовая анкета (.PDF)</p>
+                        <p className="Doc_list">4) Документ, подтверждающий факт оплаты (.PDF)</p>
+                        <p className="Doc_list">5) Удостоверение личности(.PDF)</p>
+                        <p className="Doc_list">6) Перевод удостоверения личности(.PDF)</p>
+                        <input type="file" id="actual-btn" hidden/>
+                        <label for="actual-btn" className="label_doc"> Выберите файлы <InsertDriveFileIcon sx={{ fontSize: 20 }}/></label>
+                    </div>
+                </div>
+            </div>
+                <label className="checkbox_style_contract">
+                    <input type="checkbox" onClick={handleClickContract}/> Вы уверены, что хотите добавить студента?
+                </label>
+                <div className="button_position_contract_doc">
+                <button type="submit" className="button_style_contract_doc"  disabled={active}>Добавить</button>
             </div>
             </>
+
+
             )
 }
 
--- client/src/components/common/Quota/Quota.jsx
+++ client/src/components/common/Quota/Quota.jsx
@@ -1,48 +1,18 @@
 import React from 'react';
 import './Quota.css';
-import { useState } from 'react';
+
 import QuotaDoc from './QuotaDoc';
-import QuotaEducation from './QuotaEducation';
-import QuotaInfo from './QuotaInfo';
+
 
 
 
 function Quota() {
 
-    const [isActiveInfo, setIsActiveInfo] = useState(false);
-    const [isActiveEducation, setIsActiveEducation] = useState(true);
-    const [isActiveDoc, setIsActiveDoc] = useState(true);
-    
-    const handleClickInfo = () => {
-        setIsActiveInfo(false)
-        setIsActiveEducation(true);
-        setIsActiveDoc(true);
-       };
 
-    const handleClickEducation = () => {
-        setIsActiveInfo(true)
-        setIsActiveEducation(false);
-        setIsActiveDoc(true);
-    };
-
-    const handleClickDoc = () => {
-        setIsActiveInfo(true)
-        setIsActiveEducation(true);
-        setIsActiveDoc(false);
-    };
 
     return (
             <>
-            <div className="nav_quota">
-                <button onClick={handleClickInfo} type="button" disabled={isActiveDoc && isActiveEducation}  className="button_style_subnav">Личная информация</button>
-                <button onClick={handleClickEducation} type="button" disabled={isActiveDoc && isActiveInfo} className="button_style_subnav">Образование</button>
-                <button onClick={handleClickDoc} type="button" disabled={isActiveInfo && isActiveEducation} className="button_style_subnav">Документы и оплата</button>
-            </div>
-            { !isActiveInfo && <QuotaInfo/> }
-            
-            { !isActiveEducation && <QuotaEducation/> }
-            
-            { !isActiveDoc && <QuotaDoc/> }
+                <QuotaDoc/>
             </>
             )
 }
