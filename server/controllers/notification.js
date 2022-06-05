22558685e9d684b303c777bac43a7a6834fecba6
flexiG17 <mister23krut@gmail.com>
2022-06-05 20:28:29 +0500

    Added update notification method

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -1,5 +1,6 @@
 const Notification = require('../models/Notification')
 const database = require('../utils/database')
+const errorHandler = require('../utils/errorHandler')
 
 const databaseName = 'notifications'
 
@@ -11,20 +12,45 @@ module.exports.create = async (req, res) => {
         .then(() => {
             res.status(200).json({message: "successfully"})
         })
+        .catch(e => {
+            errorHandler(res, e)
+        })
 }
 
-module.exports.update = function (req, res){
+module.exports.update = async function (req, res) {
+    const [user] = await req.user
+    const notification = new Notification(req, user)
+
+    const condition = {id: req.query.id}
+    if (req.body.status === "Сделано") {
+        database.remove(databaseName, condition)
+            .then(() => {
+                res.status(200).send(`Так как выставлен статус ${req.body.status}, то уведомление удалено`)
+            })
+            .catch(e => {
+                errorHandler(res, e)
+            })
+    } else {
+        database.changeData(databaseName, condition, notification.getModel())
+            .then(() => {
+                res.status(200).send(`Изменения уведомления с id ${req.query.id} успешно внесены`)
+            })
+            .catch(e => errorHandler(res, e))
+    }
 }
 
-module.exports.getByUserId = async function (req, res){
+module.exports.getByUserId = async function (req, res) {
     const [user] = await req.user
 
     database.getOneField(databaseName, {user_id: user.id})
         .then(data => {
             res.status(200).send(data)
         })
+        .catch(e => {
+            errorHandler(res, e)
+        })
 }
 
-module.exports.remove = function (req, res){
+module.exports.remove = function (req, res) {
 
 }
