b414842704596b971184e17911ec0e702a24327d
flexiG17 <mister23krut@gmail.com>
2022-06-06 11:37:17 +0500

    Created page for register users

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage';
 import Massages from './Pages/Massages';
+import RegistrationPage from './Pages/RegistrationPage'
 import StartPage from './Pages/StartPage';
 import LoginPage from './Pages/LoginPage';
 import AddStudent from './Pages/AddStudent';
@@ -16,6 +17,7 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<StartPage/>}/>
                 <Route exact path="/Massages" element={<Massages/>}/>
+                <Route exact path="/RegistrationPage" element={<RegistrationPage/>}/>
                 <Route exact path="/LoginPage" element={<LoginPage/>}/>
                 <Route exact path="/HomePage" element={<HomePage/>}/>
                 <Route exact path="/AddStudent" element={<AddStudent/>}/>
---
+++ client/src/Pages/RegistrationPage.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import './RegistrationPage.css';
+import Logo from './logo.png';
+import {NavLink} from "react-router-dom";
+import {registerUser} from '../services/serverData'
+
+function RegisterPage() {
+    const [inputName, setNameItemInput] = useState('')
+    const [inputEmail, setEmailItemInput] = useState('')
+    const [inputPassword, setPasswordItemInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: inputName,
+            email: inputEmail,
+            password: inputPassword
+        }
+        registerUser(data)
+    };
+
+    return (
+        <>
+            <div className="form_container">
+                <div className="header_greeting">
+                    <div className="Register_Logo"><img src={Logo} width="250px" height="50px" alt=""/></div>
+
+
+                    <form className="form_style" onSubmit={handleSubmit}>
+                        <label className="label_style" htmlFor="name">Имя сотрудника</label>
+                        <input className="input_style" name="name" type="text" placeholder="Введите имя сотрудника"
+                               onChange={event => setNameItemInput(event.target.value)} value={inputName}/>
+                        <label className="label_style" htmlFor="email">Email</label>
+                        <input className="input_style" name="email" type="email" placeholder="Введите email сотрудника"
+                               onChange={event => setEmailItemInput(event.target.value)} value={inputEmail}/>
+                        <label className="label_style" htmlFor="password">Пароль</label>
+                        <input className="input_style" name="password" type="password"
+                               placeholder="Введите пароль сотрудника"
+                               onChange={event => setPasswordItemInput(event.target.value)} value={inputPassword}/>
+                        <label className="checkbox_style_register">
+                            <input type="checkbox" placeholder="Запомните меня"/> Запомните меня
+                        </label>
+                        <button type="submit" className="button_style_register">Зарегистрировать</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RegisterPage;
