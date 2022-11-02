c0d24255e34becfa0c0ac970671017bd345fe73e
KPEesti <iwantoseeyou1@gmail.com>
2022-11-02 21:23:56 +0500

    Переписал запрос createXlsx
    Наконец-то всё работает

--- client/src/actions/student.js
+++ client/src/actions/student.js
@@ -6,13 +6,6 @@ export function getStudents() {
     return axios.get('http://localhost:5000/api/student/', {
         headers: {Authorization: localStorage.getItem('jwt')}
     }).then(resp => resp.data)
-    // return fetch('http://localhost:5000/api/student/', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt")
-    //     },
-    // })
-    //     .then(data => data.json())
 }
 
 export function removeStudent(id) {
@@ -35,18 +28,9 @@ export function removeStudent(id) {
             color: "#FFF2ED"
         });
     })
-    // return fetch(`http://localhost:5000/api/student/remove/${id}`, {
-    //     method: 'DELETE',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt"),
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    // })
 }
 
 export function removeArrayOfStudents(data) {
-    // Не уверен, что запрос написан правильно, возможно нужно будет мануально применять JSON.stringify(data).
-    // Когда поднимем сервер, нужно обязательно это проверить
     return axios.delete(`http://localhost:5000/api/student/removeStudents`, {
         headers: {
             'Authorization': localStorage.getItem("jwt"),
@@ -67,14 +51,6 @@ export function removeArrayOfStudents(data) {
             color: "#FFF2ED"
         });
     })
-    // return fetch(`http://localhost:5000/api/student/removeStudents`, {
-    //     method: 'DELETE',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt"),
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    //     body: JSON.stringify(data)
-    // })
 }
 
 export function changeStudentData(item, id) {
@@ -98,14 +74,6 @@ export function changeStudentData(item, id) {
             color: "#FFF2ED"
         });
     })
-    // return fetch(`http://localhost:5000/api/student/update/${id}`, {
-    //     method: 'PATCH',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt"),
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    //     body: JSON.stringify(item)
-    // })
 }
 
 export function addStudent(item, navigate) {
@@ -129,44 +97,16 @@ export function addStudent(item, navigate) {
             color: "#FFF2ED"
         });
     })
-    // return fetch('http://localhost:5000/api/student/create', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt"),
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    //     body: JSON.stringify(item)
-    // })
 }
 
 export function createXlsx(item) {
-    return axios.post('http://localhost:5000/api/student/getXlsxToDownload', item, {
+    return axios.post('http://localhost:5000/api/student/download/xlsx', item, {
         headers: {
             'Authorization': localStorage.getItem("jwt"),
             'Content-Type': 'application/json;charset=utf-8'
-        }
-    }).then(({statusText, data}) => {
-        iziToast.success({
-            title : statusText,
-            message: data.message,
-            position: 'topRight'
-        })
-    }).catch((e) => {
-        iziToast.error({
-            title: e.response.statusText,
-            message: e.response.data.message,
-            position: "topRight",
-            color: "#FFF2ED"
-        });
+        },
+        responseType: 'blob'
     })
-    // return fetch('http://localhost:5000/api/student/getXlsxToDownload', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt"),
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    //     body: JSON.stringify(item)
-    // })
 }
 
 export function importXlsx(data) {
@@ -189,33 +129,4 @@ export function importXlsx(data) {
             color: "#FFF2ED"
         });
     })
-}
-
-export function checkFiles(files) {
-    return axios.post('http://localhost:5000/api/student/checkFiles', files, {
-        headers: {
-            'Authorization': localStorage.getItem("jwt")
-        }
-    })
-    // return fetch('http://localhost:5000/api/student/checkFiles', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt")
-    //     },
-    //     body: files
-    // })
-}
-
-export function getXlsx() {
-    return axios.get('http://localhost:5000/api/student/download/xlsx', {
-        headers: {
-            'Authorization': localStorage.getItem("jwt")
-        }
-    })
-    // return fetch('http://localhost:5000/api/student/download/xlsx', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Authorization': localStorage.getItem("jwt")
-    //     }
-    // })
 }
