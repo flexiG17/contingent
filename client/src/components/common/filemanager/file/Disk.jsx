2d33622f9c48764aa52449cb836d0502269004c9
KPEesti <iwantoseeyou1@gmail.com>
2022-12-23 16:23:33 +0500

    Добавил загрузку при в заимодействии с сервером в ФМ

--- client/src/components/common/filemanager/file/Disk.jsx
+++ client/src/components/common/filemanager/file/Disk.jsx
@@ -3,14 +3,16 @@ import './Disk.css';
 import FileList from "./FileList/FileList";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchFilesAction, uploadFile} from "../../../../store/api-actions";
-import {getCurrentDir, getDirStack} from "../../../../store/slices/ManagerData/selectors";
+import {getCurrentDir, getDirStack, getIsLoading} from "../../../../store/slices/ManagerData/selectors";
 import ModalDirectory from "./ModalDirectory";
 import {popDirStack, setCurrentDir} from "../../../../store/slices/ManagerData/manager-data";
+import Spinner from "../../Spinner";
 
 const Disk = ({studentId}) => {
     const dispatch = useDispatch();
     const currentDir = useSelector(getCurrentDir);
     const dirStack = useSelector(getDirStack);
+    const isLoading = useSelector(getIsLoading);
 
     const [active, setActive] = useState(false);
 
@@ -35,21 +37,26 @@ const Disk = ({studentId}) => {
 
     return (
         <>
-            <div className="disk">
-                <div className="disk_btns">
-                    <button className="disk_style_btns" onClick={backClickHandler}>Назад</button>
-                    <button className="disk_style_btns" onClick={openCreateDir}>Добавить папку</button>
-                    <div className="disk_style_btns">
-                        <label htmlFor="disk_upload_id">Выберете файл</label>
-                        <input type="file" id="disk_upload_id" style={{display: "none"}}
-                               onChange={uploadFileHandler} multiple={true}/>
+            {
+                isLoading ?
+                    <Spinner/>
+                    :
+                    <div className="disk">
+                        <div className="disk_btns">
+                            <button className="disk_style_btns" onClick={backClickHandler}>Назад</button>
+                            <button className="disk_style_btns" onClick={openCreateDir}>Добавить папку</button>
+                            <div className="disk_style_btns">
+                                <label htmlFor="disk_upload_id">Выберете файл</label>
+                                <input type="file" id="disk_upload_id" style={{display: "none"}}
+                                       onChange={uploadFileHandler} multiple={true}/>
+                            </div>
+                        </div>
+                        <FileList/>
+                        {
+                            active && <ModalDirectory active={active} setActive={setActive} studentId={studentId}/>
+                        }
                     </div>
-                </div>
-                <FileList/>
-                {
-                    active && <ModalDirectory active={active} setActive={setActive} studentId={studentId}/>
-                }
-            </div>
+            }
         </>
     )
 }
