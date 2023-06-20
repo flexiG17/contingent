92e22c512a9684bad6e90f82996805416699838b
flexiG <mister23krut@gmail.com>
2023-06-20 22:15:07 +0500

    Заменил тэг <a/> на <Link/> при отображении основного лого

--- client/src/components/common/Header/index.jsx
+++ client/src/components/common/Header/index.jsx
@@ -3,6 +3,7 @@ import {Navbar} from '../../common';
 import './Header.css';
 import Logo from '../Header/full_logo.png';
 import {HOME_ROUTE} from "../../../utils/consts/pathRoutes";
+import {Link} from "react-router-dom";
 
 
 export default function Header() {
@@ -11,7 +12,7 @@ export default function Header() {
         <section className="header">
             <section className="header-top">
                 <section className="header-top__logo">
-                    <a href={HOME_ROUTE} className="header-logo"><img src={Logo} width="150px" height="108,7px" alt=""/></a>
+                    <Link to={HOME_ROUTE} className="header-logo"><img src={Logo} width="150px" height="108,7px" alt=""/></Link>
                 </section>
                 <section className="header-top__navigation">
                     <Navbar/>
