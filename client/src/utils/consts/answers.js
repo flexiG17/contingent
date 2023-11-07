03c8679f69575cc599372abbeab4a16e1d56b9d0
flexiG <mister23krut@gmail.com>
2023-11-07 19:51:42 +0500

    Добавлены уникальные значения для полей с заранее заготовленными ответами

--- client/src/utils/consts/answers.js
+++ client/src/utils/consts/answers.js
@@ -2,146 +2,194 @@ export const Answers = {
     gender: {
         label: 'Пол',
         type: 'string',
-        answers: ['Мужской', 'Женский']
+        answers: [
+            {
+                id: 1,
+                value: 'Мужской'
+            },
+            {
+                id: 2,
+                value: 'Женский'
+            }
+        ]
     },
     RF_location: {
         label: 'Нахождение в РФ',
         type: 'string',
-        answers: ['Да', 'Нет']
+        answers: [
+            {
+                id: 1,
+                value: 'Да'
+            },
+            {
+                id: 2,
+                value: 'Нет'
+            }
+        ]
     },
     started_learning: {
         label: 'Приступил к обучению',
         type: 'string',
-        answers: ['Да', 'Нет']
+        answers: [
+            {
+                id: 1,
+                value: 'Да'
+            },
+            {
+                id: 2,
+                value: 'Нет'
+            }
+        ]
     },
     hours_number: {
         name: 'hours_number',
         label: 'Количество часов',
         type: 'string',
         answers: [
-            '1008 (1.5 года 24-25 г.)',
-            '1008 (1 год 23-24 г.)',
-            '1008 (1.5 года 23-24 г.)',
-            '868 (23-24 уч.г.)',
-            '728 (23-24 уч.г.)',
-            '588 (23-24 уч.г.)',
-            '504 (23-24 уч.г.)',
-            '352 (23-24 уч.г.)',
-            '288 (23-24 уч.г.)',
-            '144 (23-24 уч.г.)',
-            '108 (23-24 уч.г.)',
-            '72 (23-24 уч.г.)'
+            {
+                id: 1,
+                value: '1008 (1.5 года 24-25 г.)'
+            },
+            {
+                id: 2,
+                value: '1008 (1 год 23-24 г.)'
+            },
+            {
+                id: 3,
+                value: '1008 (1.5 года 23-24 г.)'
+            },
+            {
+                id: 4,
+                value: '868 (23-24 уч.г.)'
+            },
+            {
+                id: 5,
+                value: '728 (23-24 уч.г.)'
+            },
+            {
+                id: 6,
+                value: '588 (23-24 уч.г.)'
+            },
+            {
+                id: 7,
+                value: '504 (23-24 уч.г.)'
+            },
+            {
+                id: 8,
+                value: '352 (23-24 уч.г.)'
+            },
+            {
+                id: 9,
+                value: '288 (23-24 уч.г.)'
+            },
+            {
+                id: 10,
+                value: '144 (23-24 уч.г.)'
+            },
+            {
+                id: 11,
+                value: '108 (23-24 уч.г.)'
+            },
+            {
+                id: 12,
+                value: '72 (23-24 уч.г.)'
+            }
         ]
     },
     form_study: {
         label: 'Форма обучения',
         type: 'string',
-        answers: ['Очная', 'Гибрид', 'Онлайн']
+        answers: [
+            {
+                id: 1,
+                value: 'Очная'
+            },
+            {
+                id: 2,
+                value: 'Гибрид'
+            },
+            {
+                id: 3,
+                value: 'Онлайн'
+            }
+        ]
     },
     education_type: {
         label: 'Тип обучения',
         type: 'string',
-        answers: ['Контракт', 'Квота']
+        answers: [
+            {
+                id: 1,
+                value: 'Контракт'
+            },
+            {
+                id: 2,
+                value: 'Квота'
+            }
+        ]
     },
     enrollment: {
         label: 'Статус зачисления',
         type: 'string',
-        answers: ['Зачислен', 'Не зачислен', 'Отчислен']
+        answers: [
+            {
+                id: 1,
+                value: 'Зачислен'
+            },
+            {
+                id: 2,
+                value: 'Не зачислен'
+            },
+            {
+                id: 3,
+                value: 'Отчислен'
+            }
+        ]
     },
     status_1c: {
         label: 'Статус 1C',
         type: 'string',
-        answers: ['Прикреплен', 'Не прикреплен']
+        answers: [
+            {
+                id: 1,
+                value: 'Прикреплен'
+            },
+            {
+                id: 2,
+                value: 'Не прикреплен'
+            }
+        ]
     },
     payment_status: {
         label: 'Статус оплаты',
         type: 'string',
-        answers: ['Оплачено', 'Не оплачено', 'Оплачено частично']
+        answers: [
+            {
+                id: 1,
+                value: 'Оплачено'
+            },
+            {
+                id: 2,
+                value: 'Не оплачено'
+            },
+            {
+                id: 3,
+                value: 'Оплачено частично'
+            }
+        ]
     },
     scholarship: {
         label: 'Стипендия',
         type: 'string',
-        answers: ['Да', 'Нет']
+        answers: [
+            {
+                id: 1,
+                value: 'Да'
+            },
+            {
+                id: 2,
+                value: 'Нет'
+            }
+        ]
     }
-}
-
-export const Gender = {
-    name: 'gender',
-    label: 'Пол',
-    type: 'string',
-    answers: ['Мужской', 'Женский']
-}
-
-export const Location = {
-    name: 'RF_location',
-    label: 'Нахождение в РФ',
-    type: 'string',
-    answers: ['Да', 'Нет']
-}
-
-export const StartedLearned = {
-    name: 'started_learning',
-    label: 'Приступил к обучению',
-    type: 'string',
-    answers: ['Да', 'Нет']
-}
-
-export const HoursNumber = {
-    name: 'hours_number',
-    label: 'Количество часов',
-    type: 'string',
-    answers: [
-        '1008 (1 год 23-24)',
-        '1008 (1.5 года 23-24)',
-        '868',
-        '728',
-        '588',
-        '504',
-        '288',
-        '144',
-        '108',
-        '72'
-    ]
-}
-
-export const FormStudy = {
-    name: 'form_study',
-    label: 'Форма обучения',
-    type: 'string',
-    answers: ['Очная', 'Гибрид', 'Онлайн']
-}
-
-export const EducationType = {
-    name: 'education_type',
-    label: 'Тип обучения',
-    type: 'string',
-    answers: ['Контракт', 'Квота']
-}
-
-export const Enrollment = {
-    name: 'enrollment',
-    label: 'Статус зачисления',
-    type: 'string',
-    answers: ['Зачислен', 'Не зачислен', 'Отчислен']
-}
-
-export const Status1C = {
-    name: 'status_1c',
-    label: 'Статус 1C',
-    type: 'string',
-    answers: ['Прикреплен', 'Не прикреплен']
-}
-
-export const PaymentStatus = {
-    name: 'payment_status',
-    label: 'Статус оплаты',
-    type: 'string',
-    answers: ['Оплачено', 'Не оплачено', 'Оплачено частично']
-}
-
-export const Scholarship = {
-    name: 'scholarship',
-    label: 'Стипендия',
-    type: 'string',
-    answers: ['Да', 'Нет']
 }
